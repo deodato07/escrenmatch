db7ebe9969a56d47fcdf9daa2f08c4e60e2f7610
Deodato Moreira <deodatomoreira07@gmail.com>
2023-05-23 15:50:55 -0300

    Criando novos metodos

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,28 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    public int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+   public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duracao EmMinutos: " + duracaoEmMinutos);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+}
